0750f5561fe27d913182935dfec04e2a1039196a
Nadia Jeftha <nadiajeftha2@gmail.com>
2024-11-09 17:41:59 +0200

    added breaks

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ store.dispatch = function (action) {
   switch (action.type) {
     case "ADD":
       this.state = { count: this.state.count + 1 };
+      break
     case "SUBSTRACT":
       this.state = { count: this.state.count - 1 };
+      break
     case "RESET":
       this.state = { count: 0 };
+      break
+      default
+      break
   }
 };
